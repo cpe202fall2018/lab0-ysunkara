e480a297c11040041380cfd6b0e4393dbbc05087
Yogi Sunkara <sunkara.yogitha@gmail.com>
2018-09-23 22:32:20 -0700

    Updated program to use only one cast and spacing

--- planets.py
+++ planets.py
@@ -1,6 +1,8 @@
 def weight_on_planets():
-weight = int(input('What do you weigh on earth?', end=''))
-print("On Mars you would weigh"+ weight*0.38 + " pounds."+ "\n On Jupiter wou would weigh" + weight*2.34 +" pounds." )
+    weight = float(input('What do you weigh on earth? '))
+    marsWeight=weight*0.38
+    juptierWeight= weight*2.34
 
+    print("\nOn Mars you would weigh ", marsWeight, "\nOn Jupiter you would weigh", juptierWeight)
 if __name__ == '__main__':
    weight_on_planets()
